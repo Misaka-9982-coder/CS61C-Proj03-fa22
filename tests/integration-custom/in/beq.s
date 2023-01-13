0b9ae57ddec78e5ad876077c65733bcda83c9b75
misaka <1784321894@qq.com>
2023-01-13 11:12:31 +0800

    CS61C Proj03: Task 7.3 Datapath beq finish

---
+++ tests/integration-custom/in/beq.s
@@ -0,0 +1,6 @@
+         addi s1, x0, 1
+         addi s2, x0, 2
+         beq  s1, s1, label1
+         addi s0, x0, 1
+label1:  addi s0, x0, 1
+         addi s0, x0, 2
--- tests/integration-custom/in/branch.s
+++
@@ -1,7 +0,0 @@
-         addi s1, x0, 1
-         addi s0, x0, -1
-         addi s0, x0, 1
-         beq  s0, s0, label1
-         addi s0, x0, 1
-label1:  addi s0, x0, 1
-         addi s0, x0, 2
